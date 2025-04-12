4708f17a59961f95a2e835dbd071faa3f76aa01f
mutheeEverlyn <muthee.14952@students.kyu.ac.ke>
2025-04-12 16:05:56 +0300

    added tables

--- bookstore.sql
+++ bookstore.sql
@@ -3,7 +3,44 @@
 CREATE DATABASE  bookstore_db;
 USE bookstore_db;
 
+-- book_language table
+CREATE TABLE book_language (
+    language_id INT AUTO_INCREMENT PRIMARY KEY,
+    language_name VARCHAR(50) NOT NULL
+);
 
+-- publisher table
+CREATE TABLE publisher (
+    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
+    publisher_name VARCHAR(255) NOT NULL
+);
+
+-- book table
+CREATE TABLE book (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    publisher_id INT,
+    language_id INT,
+    price DECIMAL(10,2),
+    FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id),
+    FOREIGN KEY (language_id) REFERENCES book_language(language_id)
+);
+
+-- author table
+CREATE TABLE author (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100)
+);
+
+-- book_author table
+CREATE TABLE book_author (
+    book_id INT,
+    author_id INT,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES book(book_id),
+    FOREIGN KEY (author_id) REFERENCES author(author_id)
+);
 
 
 -- shipping_method table
@@ -53,6 +90,7 @@ CREATE TABLE order_history (
 );
 
 
+
 -- Create Admin User with Full access
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'adminspass123!';
 GRANT ALL PRIVILEGES ON bookstore_db.* TO 'admin'@'localhost';
@@ -70,7 +108,4 @@ CREATE USER 'shippingstaff'@'localhost' IDENTIFIED BY 'ShipstaffPass123!';
 GRANT SELECT, UPDATE ON bookstore_db.cust_order TO 'shippingstaff'@'localhost';
 GRANT SELECT, UPDATE ON bookstore_db.order_status TO 'shippingstaff'@'localhost';
 GRANT SELECT, INSERT ON bookstore_db.order_history TO 'shippingstaff'@'localhost';
-
-
-
 
